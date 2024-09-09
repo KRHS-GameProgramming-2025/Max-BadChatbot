4234cfde1e94c39b697f34a8db073f045de1e89d
Max <Maximus.grey@kearsarge.org>
2024-09-09 11:38:46 -0400

    setup

---
+++ Hello.py
@@ -0,0 +1,24 @@
+name= input( " What Is your name? ")
+greeting = "Hello"
+print (greeting+ " " + name)
+food = input ( "What is yor favorite food? ")
+if food == " Pizza ":
+    print("I like pizza too!")
+elif food == "pizza":
+    print("I like pizza too!")
+elif food == "Mac and Cheese":
+    print("WOW "+ name + ",same!")
+if food == "Salad":
+     print("Dont talk to me "+ name)
+elif food == "salad":
+    print("Dont talk to me "+ name)
+if food == "Burger":
+    print("Thats my favorite food!")
+elif food == "burger":
+    print("Thats my favorite food!")
+else:
+    print("I like " + food +" too ")
+
+food = input("Lets talk about something else.Whats your favorite Video Game")
+if game == "Fortnite":
+    print("What are you nine?")
